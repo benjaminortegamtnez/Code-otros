9a3e32dfca846742e9493cdfb72fbf67c7f919b4
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2023-08-18 11:41:08 -0600

    Se agrega el ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); /* agregué punto */
+const $b = document.querySelector('.blog'); /* Elimine el #, agregué punto */
+const $l = document.querySelector('.location'); /* agregué punto */
 
-function displayUser(username) {
+async /* agregue async */function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json() /* faltaba esta linea de codigo para que se pueda realizar el async */
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; /* uso incorrecto de comillas  */
+  $b.textContent = `${data.blog}`; /* uso incorrecto de comillas */
+  $l.textContent = `${data.location}`;  /* uso incorrecto de comillas */
 }
 
 function handleError(err) {
